fac9c26da18c1fd1b4a75c12b5f6951a777dc204
smr930 <razasyedm@yahoo.com>
2021-01-18 10:01:31 -0500

    add ImageLinkForm, add css background

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import './App.css';
 import 'tachyons';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 function App() {
     return (
         <div className="App">
             <Navigation />
             <Logo />
-            {/*
             <ImageLinkForm />
+            {/*
+            
             <FaceRecognition /> */}
         </div>
     );
